27f4998c113a494e3e833bebf0da643dce8064a8
TaejungKim <tae0803@naver.com>
2024-05-08 15:35:27 +0900

    Initial commit

---
+++ app/src/main/java/com/example/emergencyguide/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.emergencyguide
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.emergencyguide.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding : ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+
+        setContentView(binding.root)
+    }
+}
