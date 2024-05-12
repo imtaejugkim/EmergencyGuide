a49c35e24d60e8b81f8333cf44fd9d54dff2cde9
TaejungKim <tae0803@naver.com>
2024-05-12 13:50:24 +0900

    feat: 메인화면 및 각 화면 연결
    메인화면 구축했습니다.
    각 메인화면 순서대로 가이드, 병원 및 약국 위치, 대피소, 긴급 연락처, 복용법 엑티비티로 이동합니다.

--- app/src/main/java/com/example/emergencyguide/MainActivity.kt
+++ app/src/main/java/com/example/emergencyguide/MainActivity.kt
@@ -1,7 +1,12 @@
 package com.example.emergencyguide
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.emergencyguide.EmergencyGuide.EmergencyGuideActivity
+import com.example.emergencyguide.EmergencyNumber.EmergencyNumberActivity
+import com.example.emergencyguide.HospitalPharmacy.HospitalActivity
+import com.example.emergencyguide.MedicineGuide.MedicineActivity
 import com.example.emergencyguide.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,6 +15,30 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
 
+        binding.clMainEmergencyGuide.setOnClickListener {
+            val intent = Intent(this, EmergencyGuideActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.clMainEmergencyNumber.setOnClickListener {
+            val intent = Intent(this, EmergencyNumberActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.clMainHospital.setOnClickListener {
+            val intent = Intent(this, HospitalActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.clMainMedicine.setOnClickListener {
+            val intent = Intent(this, MedicineActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.clMainEmergencyNumber.setOnClickListener {
+            val intent = Intent(this, EmergencyNumberActivity::class.java)
+            startActivity(intent)
+        }
 
         setContentView(binding.root)
     }
