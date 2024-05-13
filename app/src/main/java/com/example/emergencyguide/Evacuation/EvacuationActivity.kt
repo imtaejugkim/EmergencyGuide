ce6382a446b3ff16e9fd2ba83718942d18119e04
slr-09 <ge12090@naver.com>
2024-05-13 16:48:23 +0900

    feat(evacuation): 뒤로가기 기능 추가

--- app/src/main/java/com/example/emergencyguide/Evacuation/EvacuationActivity.kt
+++ app/src/main/java/com/example/emergencyguide/Evacuation/EvacuationActivity.kt
@@ -1,21 +1,35 @@
 package com.example.emergencyguide.Evacuation
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.emergencyguide.R
+import com.example.emergencyguide.databinding.ActivityEvacuationBinding
 
 class EvacuationActivity : AppCompatActivity() {
+    lateinit var binding: ActivityEvacuationBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityEvacuationBinding.inflate(layoutInflater)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_evacuation)
+
+        setListeners()
+
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+    }
+
+    private fun setListeners() {
+        // back button 클릭 시 activity 종료
+        binding.ivEvaBack.setOnClickListener {
+            finish()
         }
     }
 }
