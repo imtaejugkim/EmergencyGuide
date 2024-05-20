a7996c15358029480494ea33d2453bb9ad782193
TaejungKim <tae0803@naver.com>
2024-05-20 13:40:44 +0900

    fix: revert emergency number 파트
    :checkered_flag: emergency number 파트 revert flag 1

--- app/src/main/java/com/example/emergencyguide/EmergencyNumber/EmergencyNumber.kt
+++
@@ -1,7 +0,0 @@
-package com.example.emergencyguide.EmergencyNumber
-
-data class EmergencyNumber (
-    var title: String = "",
-    var information: String = "",
-    var isExpanded: Boolean = false
-)
--- app/src/main/java/com/example/emergencyguide/EmergencyNumber/EmergencyNumberActivity.kt
+++ app/src/main/java/com/example/emergencyguide/EmergencyNumber/EmergencyNumberActivity.kt
@@ -1,22 +1,13 @@
 package com.example.emergencyguide.EmergencyNumber
 
-
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-
 import com.example.emergencyguide.R
 
 class EmergencyNumberActivity : AppCompatActivity() {
-
-
-    private lateinit var emergencyNumberList: List<EmergencyNumber>
-    private lateinit var adapter: EmergencyNumberExpandableAdapter
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -26,33 +17,5 @@ class EmergencyNumberActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-        val recyclerView = findViewById<RecyclerView>(R.id.emergency_number_list)
-
-        emergencyNumberList = ArrayList()
-        emergencyNumberList = loadData()
-
-        recyclerView.setHasFixedSize(true)
-        recyclerView.layoutManager = LinearLayoutManager(this)
-        adapter = EmergencyNumberExpandableAdapter(emergencyNumberList)
-        recyclerView.adapter = adapter
-    }
-
-    private fun loadData() : List<EmergencyNumber> {
-        val emergencyNumberList = ArrayList<EmergencyNumber>()
-
-        val emergencyNumbers = resources.getStringArray(R.array.emergency_number_title)
-        val informations = resources.getStringArray(R.array.emergency_number_information)
-
-        for (i in emergencyNumbers.indices) {
-            val emergencyNumber = EmergencyNumber().apply {
-                title = emergencyNumbers[i]
-                information = informations[i]
-            }
-            emergencyNumberList.add(emergencyNumber)
-        }
-        return emergencyNumberList
     }
 }
-
-
