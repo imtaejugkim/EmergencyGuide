c6e8b863b1143d68311276e6c13bd601174302fe
MaBeing <john9912@naver.com>
2024-06-02 20:48:37 +0900

    설명 추가

--- app/src/main/java/com/example/emergencyguide/EmergencyGuide/EmergencyGuideActivity.kt
+++ app/src/main/java/com/example/emergencyguide/EmergencyGuide/EmergencyGuideActivity.kt
@@ -125,24 +125,37 @@ class EmergencyGuideActivity : AppCompatActivity() {
                 title = "심정지",
                 content = "심폐소생술(CPR)",
                 steps = listOf(
-                    StepData(R.drawable.ic_earthquake, "CPR Step 1", listOf("Description 1.1", "Description 1.2")),
-                    StepData(R.drawable.ic_earthquake, "CPR Step 2", listOf("Description 2.1", "Description 2.2"))
+                    StepData(R.drawable.ic_earthquake, "Step 1", listOf("심정지 및 무호흡 확인", "양어깨를 두드리며 말을 걸고 눈과 귀로 심정지 및 무호흡 유무를 확인한다.")),
+                    StepData(R.drawable.ic_earthquake, "Step 2", listOf("도움 및 119신고 요청", "주변사람에게(꼭 집어서) 119신고를 부탁하고 자동심장충격기를 요청한다.")),
+                    StepData(R.drawable.ic_earthquake, "Step 3", listOf("가슴압박 30회 시행", "환자의 가슴 중앙에 깍지낀 두손으로 몸과 수직이 되도록 압박한다.")),
+                    StepData(R.drawable.ic_earthquake, "Step 4", listOf("인공호흡 2회 시행", "코를 막고 구조자의 입을 완전히 밀착하여 정상호흡을 약 1초에 걸쳐 2회 숨을 불어 넣는다.")),
+                    StepData(R.drawable.ic_earthquake, "Step 5", listOf("가슴압박, 인공호흡 반복", "이후에는 30회의 가슴압박과 2회의 인공호흡을 119구급대원이 현장에 도착할 때까지 반복해서 시행한다."))
                 )
             ),
             ExpandableBoxData(
                 title = "기도폐쇄/목에 걸림",
                 content = "하임리히법(Heimlich)",
                 steps = listOf(
-                    StepData(R.drawable.ic_earthquake, "Heimlich Step 1", listOf("Description 1.1", "Description 1.2")),
-                    StepData(R.drawable.ic_earthquake, "Heimlich Step 2", listOf("Description 2.1", "Description 2.2")),
-                    StepData(R.drawable.ic_earthquake, "Heimlich Step 3", listOf("Description 3.1", "Description 3.2"))
+                    StepData(R.drawable.ic_earthquake, "Step 1", listOf("상태체크 및 119신고 요청", "환자가 숨쉬기 힘들어 하거나 목을 감싸 괴로움을 호소할 경우 기도폐쇄로 판단하고 주변에 119에 신고를 요청한다.")),
+                    StepData(R.drawable.ic_earthquake, "Step 2", listOf("하임리히법 실시", "환자의 등 뒤에 서서 주먹을 쥔 손의 엄지손가락 방향을 배 윗부분에 대고 다른 한 손을 위에 겹친 후 환자의 배꼽에서 명치 사이의 배 부위를 두 손으로 위로 쓸어올리듯 강하게 밀어 올려서 이물을 제거하고 이물이 밖으로 나왔는지 확인한다."))
                 )
             ),
             ExpandableBoxData(
-                title = "화상",
-                content = "화상 치료법",
+                title = "열상화상",
+                content = "열상화상 응급 처지",
                 steps = listOf(
-                    StepData(R.drawable.ic_earthquake, "Burn Step 1", listOf("Description 1.1", "Description 1.2"))
+                    StepData(R.drawable.ic_earthquake, "Step 1", listOf("화상 부위를 찬물에 20분 이상 담가 열기를 식힌다.", "뜨거운 액체에 화상을 입은 경우 옷을 벗기지 않고 냉각시킨다.")),
+                    StepData(R.drawable.ic_earthquake, "Step 2", listOf("물집은 절대 터뜨리지 말고 로션, 된장, 간장, 소주 등도 절대 바르지 않는다", "시계, 반지, 목걸이 등의 장신구는 피부가 부어오르기 전에 최대한 빨리 제거한다.")),
+                    StepData(R.drawable.ic_earthquake, "Step 3", listOf("화상 부위에 바세린이나 화상 거즈(깨끗한 거즈)로 덮어주고 붕대로 감아준다.", ""))
+                )
+            ),
+            ExpandableBoxData(
+                title = "벌에 쏘인 경우",
+                content = "독침 제거",
+                steps = listOf(
+                    StepData(R.drawable.ic_earthquake, "Step 1", listOf("벌침 찾기", "빨갛게 부어오른 부위에 검은 점처럼 보이는 벌침을 찾는다.")),
+                    StepData(R.drawable.ic_earthquake, "Step 2", listOf("벌침 제거", "신용카드 등을 이용해 피부를 긁어내듯 침을 제거한다.")),
+                    StepData(R.drawable.ic_earthquake, "Step 3", listOf("통증(부기) 완화", "상처 부위에 얼음주머니를 대 통증과 부기를 가라앉힌다."))
                 )
             )
         )
